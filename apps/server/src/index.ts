d5c5994a4aed79762610c4b7af262d02e4315651
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2026-06-21 16:19:07 +0700

    feat: error page, SPA-only

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,15 @@
+import { cors } from "@elysia/cors";
 import { node } from "@elysia/node";
 import { Elysia } from "elysia";
 
 const app = new Elysia({ adapter: node() })
   .get("/", () => "Hello Elysia")
+  .use(
+    cors({
+      // Accept all, this app has nothing to secure from
+      origin: true,
+    }),
+  )
   .listen(5001, ({ hostname, port }) => {
     console.log(`Elysia is running at ${hostname}:${port}`);
   });
